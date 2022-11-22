2fdc2accdaaea78b5ac91908c626b8f334693b47
Sam Flamini <saflamini@gmail.com>
2022-11-21 18:02:02 -0600

    fix hardhat.config in tcf

--- projects/tradeable-cashflow/hardhat.config.js
+++ projects/tradeable-cashflow/hardhat.config.js
@@ -6,7 +6,7 @@ require("@nomiclabs/hardhat-etherscan");
 
  require("dotenv").config();
  const GAS_LIMIT = 10000000;
-  const defaultNetwork = 'goerli';
+  // const defaultNetwork = 'goerli';
 
 module.exports = {
     solidity: {
@@ -20,16 +20,16 @@ module.exports = {
     namedAccounts: {
         deployer: 0
     },
-    networks: {
-    goerli: {
-      url: `${process.env.GOERLI_RPC_URL}`,
-      accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
-      gas: GAS_LIMIT,
+    // networks: {
+    // goerli: {
+    //   url: `${process.env.GOERLI_RPC_URL}`,
+    //   accounts: [`0x${process.env.DEPLOYER_PRIVATE_KEY}`],
+    //   gas: GAS_LIMIT,
     //   gasPrice: 11e9, // 10 GWEI
     //   confirmations: 6, // # of confs to wait between deployments. (default: 0)
     //   timeoutBlocks: 50, // # of blocks before a deployment times out  (minimum/default: 50)
     //   skipDryRun: false // Skip dry run before migrations? (default: false for public nets )
-      },
-    }
+      // },
+    // }
     
 }
