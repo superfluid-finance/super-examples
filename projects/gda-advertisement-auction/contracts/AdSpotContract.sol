85bca03298c06e0009e58045e0df3e96c2b64dcc
youssefea <youcefea99@gmail.com>
2023-11-16 19:11:30 +0000

    update contract

--- projects/gda-advertisement-auction/contracts/AdSpotContract.sol
+++ projects/gda-advertisement-auction/contracts/AdSpotContract.sol
@@ -9,7 +9,7 @@ pragma solidity >=0.8.2 <0.9.0;
  * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
 
-import {ISuperfluid, ISuperToken, ISuperApp} from "./contracts/interfaces/superfluid/ISuperfluid.sol";
+import {ISuperfluid, ISuperToken, ISuperApp, SuperAppDefinitions} from "./contracts/interfaces/superfluid/ISuperfluid.sol";
 import {ISuperfluidPool} from "./contracts/interfaces/agreements/gdav1/ISuperfluidPool.sol";
 import {SuperTokenV1Library} from "./contracts/apps/SuperTokenV1Library.sol";
 import {SuperAppBaseFlow} from "./contracts/apps/SuperAppBaseFlow.sol";
@@ -25,7 +25,7 @@ contract AdSpotContract is SuperAppBaseFlow {
     ISuperfluidPool pool;
     ISuperToken acceptedToken;
     address private owner;
-    address private highestBidder;
+    address public highestBidder;
     int96 private highestFlowRate;
     uint private lastUpdate;
     PoolConfig private poolConfig;
@@ -41,7 +41,7 @@ contract AdSpotContract is SuperAppBaseFlow {
         ISuperToken _acceptedToken,
         IGeneralDistributionAgreementV1 _gda
     ) SuperAppBaseFlow(
-        ISuperfluid(_acceptedToken.getHost()), 
+        ISuperfluid(ISuperToken(_acceptedToken).getHost()), 
         true, 
         true, 
         true,
@@ -57,7 +57,6 @@ contract AdSpotContract is SuperAppBaseFlow {
         highestFlowRate=acceptedToken.getFlowRate(owner, address(this));
         lastUpdate=block.timestamp;
         highestBidder=address(0);
-
     }
 
     function isAcceptedSuperToken(ISuperToken superToken) public view override returns (bool) {
@@ -68,7 +67,7 @@ contract AdSpotContract is SuperAppBaseFlow {
     // SUPER APP CALLBACKS
     // ---------------------------------------------------------------------------------------------
 
-    /**
+    /*
      * @dev Callback function that gets executed when a new flow is created to this contract.
      *      It handles logic for updating the highest bidder and distributing shares.
      * @param sender The address of the sender creating the flow.
@@ -79,24 +78,26 @@ contract AdSpotContract is SuperAppBaseFlow {
         ISuperToken /*superToken*/,
         address sender,
         bytes calldata ctx
-    ) internal override returns (bytes memory) {
+    ) internal override returns (bytes memory newCtx) {
         int96 senderFlowRate= acceptedToken.getFlowRate(sender, address(this));
         require(senderFlowRate>highestFlowRate, "Sender flowrate lower than current flowRate");
-        bytes memory newCtx=ctx;
-        newCtx=acceptedToken.deleteFlowWithCtx(highestBidder,address(this), ctx);
+        newCtx=ctx;
+        if(highestBidder!=address(0)){
+            newCtx=acceptedToken.deleteFlowWithCtx(highestBidder,address(this), ctx);
+        }
         uint128 halfShares=uint128(block.timestamp-lastUpdate)/2;
         pool.updateMemberUnits(owner,halfShares);
         if (highestBidder!=address(0)){
             ISuperfluidPool(poolAddress).updateMemberUnits(highestBidder,halfShares);
         }
-        newCtx=gda.distributeFlow(acceptedToken, address(this),pool,senderFlowRate,newCtx);
+        newCtx=acceptedToken.distributeFlowWithCtx(pool,address(this),senderFlowRate,newCtx);
         highestBidder=sender;
         highestFlowRate=senderFlowRate;
         lastUpdate=block.timestamp;
         return newCtx;
     }
 
-    /**
+    /*
      * @dev Callback function that gets executed when an existing flow to this contract is updated.
      *      It updates the highest bidder and adjusts share distribution accordingly.
      * @param sender The address of the sender updating the flow.
@@ -111,22 +112,22 @@ contract AdSpotContract is SuperAppBaseFlow {
         int96 previousflowRate,
         uint256 lastUpdated,
         bytes calldata ctx
-    ) internal override returns (bytes memory) {
+    ) internal override returns (bytes memory newCtx) {
         int96 senderFlowRate= acceptedToken.getFlowRate(sender, address(this));
         require(senderFlowRate>previousflowRate, "Sender flowRate is lower than the previous one");
         require(senderFlowRate>highestFlowRate, "Sender flowrate lower than current flowRate");
-        bytes memory newCtx=ctx;
+        newCtx=ctx;
         uint128 halfShares=uint128(block.timestamp-lastUpdate)/2;
         ISuperfluidPool(poolAddress).updateMemberUnits(owner,halfShares);
         ISuperfluidPool(poolAddress).updateMemberUnits(highestBidder,halfShares);
-        newCtx=gda.distributeFlow(acceptedToken, address(this),pool,senderFlowRate,newCtx);
+        newCtx=gda.distributeFlow(acceptedToken,address(this),pool,senderFlowRate,newCtx);
         highestBidder=sender;
         highestFlowRate=senderFlowRate;
         lastUpdate=block.timestamp;
         return newCtx;
     }
 
-    /**
+    /*
      * @dev Callback function that gets executed when a flow to this contract is deleted.
      *      Handles the removal of a bidder and adjustment of shares.
      * @param sender The address of the sender deleting the flow.
@@ -144,10 +145,13 @@ contract AdSpotContract is SuperAppBaseFlow {
     ) internal override returns (bytes memory newCtx) {
 
         require(sender==highestBidder, "You don't have an active stream");
-
-        /* UNFINISHED FUNCTION */
-        
-        bytes memory newCtx=ctx;
+        highestBidder=address(0);
+        uint128 halfShares=uint128(block.timestamp-lastUpdate)/2;
+        pool.updateMemberUnits(owner,halfShares);
+        if (highestBidder!=address(0)){
+            ISuperfluidPool(poolAddress).updateMemberUnits(highestBidder,halfShares);
+        }
+        newCtx=gda.distributeFlow(acceptedToken, address(this),pool,0,newCtx);
 
         return newCtx;
 
